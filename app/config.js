779f553933bdc8eda579343218272a9cb0de17b5
surya-gopilli <2023.pmemories@gmail.com>
2025-06-21 22:54:05 +0530

    Mobile app setup

---
+++ app/config.js
@@ -0,0 +1,3 @@
+export const SERVER_URL = 'https://your-render-url.onrender.com'; //replace with our render.com url
+export const AGORA_APP_ID = 'YOUR_AGORA_APP_ID'; 
+export const AGORA_TOKEN = 'YOUR_TEMP_TOKEN'; //get it from agora dashboard
